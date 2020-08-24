cb7678bc2bc1e1abc8786878f178260023250b57
Jack <jack@jackyuan.com>
2020-08-23 23:29:41 -0700

    Finished two_fer exercise

--- two-fer/lib/two_fer.dart
+++ two-fer/lib/two_fer.dart
@@ -1,3 +1,8 @@
-String twoFer() {
+String twoFer([String name]) {
   // Put your code here
+  if (name == null) {
+    return "One for you, one for me.";
+  } else {
+    return "One for $name, one for me.";
+  }
 }
