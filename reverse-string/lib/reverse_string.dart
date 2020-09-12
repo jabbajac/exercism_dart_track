be3e34d8e9df0edff564a3d26f34ff49787e67c6
Jack <jack@jackyuan.com>
2020-09-12 14:24:51 -0700

    Finishing reverse string module

--- reverse-string/lib/reverse_string.dart
+++ reverse-string/lib/reverse_string.dart
@@ -1,3 +1,8 @@
-String reverse() {
+String reverse(String strToRev) {
   // Put your code here
+  String result = "";
+  for (int i = strToRev.length; i > 0; i--) {
+    result += strToRev[i];
+  }
+  return result;
 }
